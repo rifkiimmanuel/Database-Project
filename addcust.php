85f1ba601cef71c98b2d68d53fcd1464799a97be
rifkiimmanuel <rifkiimmanuel112@gmail.com>
2023-04-01 22:32:20 +0700

    new

--- addcust.php
+++ addcust.php
@@ -24,7 +24,7 @@ if (isset($_POST['Submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Daftar Sekarang</title>
+    <title>Login now</title>
 </head>
 <body>
     <a href="indexcustomer.php">GO TO HOME</a><br><br>
